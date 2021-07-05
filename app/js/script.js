ae0365413e246b4beeb8f1398361eebe4d67267b
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-07-05 22:12:21 +0530

    feat/implemented hamburger menu transition with javascript

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,14 @@
-console.log("hello world")
+// To ensure that logic runs only after document is loaded
+window.onload = function(){
+    const btnHamburger = document.querySelector('#btnHamburger');
+    console.log('is it working??');
+    btnHamburger.addEventListener("click", function(){
+        console.log('clicked hamburger');
+        if (btnHamburger.classList.contains('open')){
+            btnHamburger.classList.remove('open');
+        }
+        else {
+            btnHamburger.classList.add('open');
+        }
+    });
+}

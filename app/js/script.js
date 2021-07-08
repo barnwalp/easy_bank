d675bc3d8eafaeef11312be1b6245dbe32b6e2eb
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-07-08 19:55:51 +0530

    feat/animation of overlay using javascript and css

--- app/js/script.js
+++ app/js/script.js
@@ -2,13 +2,21 @@
 window.onload = function(){
     const btnHamburger = document.querySelector('#btnHamburger');
     const header = document.querySelector('.header');
+    const overlay = document.querySelector('.overlay');
+
     btnHamburger.addEventListener("click", function(){
         console.log('clicked hamburger');
+        // remove overlay
         if (header.classList.contains('open')){
+            overlay.classList.add('fade-out');
+            overlay.classList.remove('fade-in');
             header.classList.remove('open');
         }
+        // open overlay
         else {
             header.classList.add('open');
+            overlay.classList.add('fade-in');
+            overlay.classList.remove('fade-out');
         }
     });
 }

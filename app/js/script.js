bdaeb4de231fdc30b740b135214deeab6a93d15e
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-07-09 21:10:29 +0530

    feat/animation of menu using javascript and css

--- app/js/script.js
+++ app/js/script.js
@@ -3,6 +3,7 @@ window.onload = function(){
     const btnHamburger = document.querySelector('#btnHamburger');
     const header = document.querySelector('.header');
     const overlay = document.querySelector('.overlay');
+    const menu = document.querySelector('.header__menu');
 
     btnHamburger.addEventListener("click", function(){
         console.log('clicked hamburger');
@@ -10,6 +11,8 @@ window.onload = function(){
         if (header.classList.contains('open')){
             overlay.classList.add('fade-out');
             overlay.classList.remove('fade-in');
+            menu.classList.add('fade-out');
+            menu.classList.remove('fade-in');
             header.classList.remove('open');
         }
         // open overlay
@@ -17,6 +20,8 @@ window.onload = function(){
             header.classList.add('open');
             overlay.classList.add('fade-in');
             overlay.classList.remove('fade-out');
+            menu.classList.add('fade-in');
+            menu.classList.remove('fade-out');
         }
     });
 }
